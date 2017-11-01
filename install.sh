f82e277b409f5253928b426f2d79a6cf14decdaf
Nate Fischer <ntfschr@google.com>
2017-10-31 23:36:02 -0700

    Simplify install script
    No change to vim config.
    
    This simplifies the install script to create all necessary symlinks and
    directories, but not to install or update any plugins (this can be done
    easily from within vim).
    
    This should also fix compatibility with Mac, since it doesn't rely on
    readarray anymore.

--- install.sh
+++ install.sh
@@ -1,76 +1,31 @@
 #!/bin/bash
 
-# Install & update all bundled vim plugins asynchronously
+VIMPLUG_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+link_if_missing() {
+  local ORIGIN="$1"
+  local DEST="$2"
+  test -L "$DEST" || ln -s "$ORIGIN" "$DEST"
+}
+
+# Always start in the current directory
 readonly BASE_DIR="$(dirname "$0")"
 cd "$BASE_DIR"
 
-# First, make sure we have the .vim subdirectories
+# Create spelling directories, etc.
 mkdir -p tmp/ spell/ undodir/
 
-# Link this directory to the neovim directory
+# Add the directory for neovim
 NVIM_DIR="$HOME/.config/nvim"
 mkdir -p "$(dirname "$NVIM_DIR")"
-test -L "$NVIM_DIR" || ln -s "$PWD" "$NVIM_DIR"
-echo "ln -s $NVIM_DIR $PWD"
+link_if_missing "$PWD" "$NVIM_DIR"
+
+# Symlink vimrc into home directory
+link_if_missing "$PWD/vimrc" "$HOME/.vimrc"
 
 # Install vim-plug
 if [ ! -f 'autoload/plug.vim' ]; then
   echo 'installing vim-plug'
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim ||
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs $VIMPLUG_URL ||
     { echo 'curl failed'; exit 1; }
 fi
-
-BUNDLE_PATH="$BASE_DIR/plugged"
-
-declare -a pids
-declare -a plugins
-declare -a dirList
-
-IFS='
-'
-readarray -t dirList <<<"$(grep '^ *Plug' vimrc |
-  egrep -v "'(pinned|frozen)' *: *1" |
-  sed "s/.*Plug\(in\)\? *'\([^/]\+\/[^']\+\)'.*/\2/")"
-echo "Updating/installing ${#dirList[@]} plugins"
-
-# Launch everything asynchronously
-mkdir -p "$BUNDLE_PATH"
-cd "$BUNDLE_PATH"
-for plugin in "${dirList[@]}"; do
-  dir="${plugin#*/}"
-  if [[ ! -e "${dir}" ]]; then
-    (
-      git clone --recursive "https://github.com/${plugin}.git"
-    ) &>/dev/null &
-    pids+=($!)
-  elif [[ -d "${dir}" ]]; then
-    # echo "Downloading ${plugin}"
-    (
-      git -C "${dir}" pull &&
-      git -C "${dir}" submodule update --init --recursive
-    ) &>/dev/null &
-    pids+=($!)
-  fi
-  plugins+=(${plugin})
-done
-
-ret=0
-# Wait for processes to finish
-for k in "${!pids[@]}"; do
-  plugin="${plugins[k]}"
-  wait "${pids[k]}"
-  rval=$?
-  if [[ $rval -eq 0 ]]; then
-    echo "Success: installed ${plugin}"
-  else
-    echo -en "\033[1;31m"
-    echo "Error installing ${plugin}" >&2
-    echo -en "\033[0m"
-    ret=$rval
-  fi
-done
-
-echo "Your plugins have been installed"
-exit ${ret}
-
